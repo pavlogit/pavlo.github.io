d6b7f9d73312fd73d0e32336ad01f05738b1a160
pavlo <pavlo.git@gmail.com>
2019-01-21 00:57:01 +0700

    portfolio

---
+++ mail.php
@@ -0,0 +1,6 @@
+<?php
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+
+echo "Приветствую тебя, " . $name . "! Твой номер телефона: " .$phone;
+?>
---
+++ mailer/smart.php
@@ -0,0 +1,45 @@
+<?php 
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$feedback = $_POST['user_feedback'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'kramola02@mail.ru';                 // Наш логин
+$mail->Password = 'pasha3214';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('kramola02@mail.ru', 'John Doe');   // От кого письмо 
+$mail->addAddress('pavlo.pht@yandex.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные пользователся с сайта';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br>
+	Телефон: ' . $phone . ' <br>
+	Отзыв: ' . $feedback . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    return false;
+} else {
+		return true;
+}
+
+?>
